e28221566f27511d1a0c762cc4e7e1a14d6a135e
John <jfberry67@gmail.com>
2022-12-01 00:17:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Our First Streamlit App')
+st.subheader('Introducing Streamlit in automate everything with Python')
+st.write('''This is our first Web App
+Enjoy it!
+''')
+
